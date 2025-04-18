a36986c62f2b0214496bc0c83c4904eb74d3f127
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-04-18 01:43:06 +0100

    parse the xsd file into json using "fast-xml-parser" package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { Hono } from "hono";
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.post("/api/v2/convert", async (c) => {
+  const body = await c.req.parseBody();
+  const file = body["xsd"] as File;
+
+  if (!file) {
+    return c.json({ ok: false, message: "Please Provide a valid file" }, 400);
+  }
+
+  const xsdContent = await file.text();
+
+  const options = {
+    ignoreAttributes: false,
+  };
+  const parser = new XMLParser(options);
+  const output = await parser.parse(xsdContent);
+
   return c.json({
     ok: true,
     message: "Hello Hono!",
+    output,
   });
 });
 
